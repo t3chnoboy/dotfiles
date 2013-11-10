c57e55a13aaf3ae37f7ff1b8924f4c1679ce68ed
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-11-10 15:30:26 +0300

    installed oh-my-fish; updated fish theme

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme bobthefish
+# set fish_theme agnoster
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+set fish_plugins autojump  brew  bundler  django  emoji-clock  fry  localhost  node  percol  php  plenv  pyenv  python  rails  rake  rbenv  rvm  sublime  z
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Powerline
+
+. /usr/local/lib/python2.7/site-packages/powerline/bindings/fish/powerline.fish
+
