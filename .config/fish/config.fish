f11dda6f2d1a8b32c6c144c2580ace157bfdcf8f
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-12-01 14:20:09 +0300

    merged fish config from OSX into Ubuntu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,29 @@ set fish_path $HOME/.oh-my-fish
 
 # Theme
 set fish_theme bobthefish
+# set fish_theme agnoster
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins  bundler emoji-clocksa  gi  localhost  node  percol  plenv  pyenv  python  rails  rake  rbenv  rvm  sublime  z
+set fish_plugins autojump brew bundler django emoji-clock fry localhost node percol php plenv pyenv python rails rake rbenv sublime z
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-# Private config
+# Powerline
+. /usr/local/lib/python2.7/site-packages/powerline/bindings/fish/powerline.fish
+
+# Private stuff
 . ~/.dotfiles/.private.fish
+
+# Aliases
+alias :q=exit
+alias :wq=exit
+alias :e=vim
+alias gs="git st"
+alias gc="git commit"
+alias gl="git lg"
+alias ga="git add"
