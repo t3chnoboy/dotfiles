250076788407fbec947363c5f6538bfedb19ba37
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-06-19 23:18:32 +0300

    add fzf functions

--- .config/fish/functions/fzf.fish
+++ .config/fish/functions/fzf.fish
@@ -1,3 +1,17 @@
 function fzf
   /usr/bin/ruby --disable-gems /Users/macuser/.fzf/fzf $argv
 end
+
+function vimf
+  if fzf > $TMPDIR/fzf.result
+    vim (cat $TMPDIR/fzf.result)
+  end
+end
+
+function fe
+  set tmp $TMPDIR/fzf.result
+  fzf --query="$argv[1]" --select-1 --exit-0 > $tmp
+  if [ (cat $tmp | wc -l) -gt 0 ]
+    vim (cat $tmp)
+  end
+end
