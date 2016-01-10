577694b29df2cfadea4f2b71504e3db159d0be63
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2016-01-10 18:12:27 +0300

    update oh-my-fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,54 +1,8 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/macuser/.local/share/omf"
 
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/macuser/.config/omf"
 
 # Load oh-my-fish configuration.
-source $fish_path/oh-my-fish.fish
-
-# Theme
-if test $TERM = eterm-color
-  Theme "l"
-else
-  # Theme "greenfish"
-  Theme 'cbjohnson'
-end
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-Plugin "brew"
-Plugin "node"
-Plugin "z"
-Plugin "tmux"
-Plugin "extract"
-Plugin "theme"
-Plugin "rbenv"
-Plugin "grc"
-# Plugin "emacs"
-
-#editor
-set -x EDITOR vim
-
-set -g __fish_git_prompt_char_dirtystate '+'
-
-# Private stuff
-source ~/.dotfiles/.private.fish
-
-# Aliases
-alias :q=exit
-alias :wq=exit
-alias :e=vim
-alias gs="git st"
-alias gc="git commit -m"
-alias gl="git lg"
-alias ga="git add"
-alias gd="git diff"
-alias gp="git push"
-alias node="node --harmony"
-alias elementExplorer="node /usr/local/lib/node_modules/protractor/bin/elementexplorer.js"
-alias rm!="rm -rf"
-alias json="underscore print --outfmt pretty"
-alias v="vim ."
-alias l="ls -f"
+source $OMF_PATH/init.fish
---
+++ .config/omf/init.fish
@@ -0,0 +1,35 @@
+# Theme
+# if test $TERM = eterm-color
+#   Theme "l"
+# else
+#   Theme 'cbjohnson'
+# end
+
+#editor
+set -x EDITOR vim
+
+set -g __fish_git_prompt_char_dirtystate '+'
+
+# Private stuff
+source ~/.dotfiles/.private.fish
+
+# Aliases
+alias :q=exit
+alias :wq=exit
+alias :e=vim
+alias gs="git st"
+alias gc="git commit -m"
+alias gl="git lg"
+alias ga="git add"
+alias gd="git diff"
+alias gp="git push"
+alias gcm="git checkout master"
+alias gst="git stash"
+alias gcb="git checkout -b"
+alias gpu="git pull"
+alias node="node --harmony"
+alias elementExplorer="node /usr/local/lib/node_modules/protractor/bin/elementexplorer.js"
+alias rm!="rm -rf"
+alias json="underscore print --outfmt pretty"
+alias v="vim ."
+alias l="ls -f"
