f13ef643582716c956f5e15ed46bb5d687fad7fe
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2016-06-27 14:41:22 +0300

    Update oh my fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 # Path to Oh My Fish install.
 set -gx OMF_PATH "/Users/macuser/.local/share/omf"
 
+source ~/.dotfiles/.private.fish
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/macuser/.config/omf"
+#set -gx OMF_CONFIG "/Users/dmitrymazuro/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
